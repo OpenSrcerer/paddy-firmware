d7e699fe919bfb638420cad560930654aa53065a
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-03-11 21:12:40 +0200

    feat: add generic storage for all required parameters supporting WPA insecure, WPA personal + enterprise

--- src/storage/StorageModule.cpp
+++ src/storage/StorageModule.cpp
@@ -6,19 +6,42 @@
 namespace paddy
 {
 
-// Checks if the first 4 chars saved in
-// Emulated EEPROM are digits. If not,
-// no JWT was saved
-bool StorageModule::hasJwt()
+bool StorageModule::hasInsecure()
 {
-	// Read the stored length of the JWT.
-	String jwtLength;
-	read(jwtLength, 0, 4);
+	return (memoryJwt.length() && memorySsid.length()) ||
+		   (hasAt(JWT_ADDRESS) && hasAt(SSID_ADDRESS));
+}
 
-	const char* length = jwtLength.c_str();
-	for (int i = 0; i < jwtLength.length(); ++i)
+bool StorageModule::hasPersonal()
+{
+	bool insecure = hasInsecure();
+	return (insecure && memoryPass.length()) ||
+		   (insecure && hasAt(PASS_ADDRESS));
+}
+
+bool StorageModule::hasEnterprise()
+{
+	bool insecure = hasInsecure();
+	return (insecure && memoryEUsername.length() && memoryEPassword.length()) ||
+		   (insecure && hasAt(E_USERNAME_ADDRESS) && hasAt(E_PASSWORD_ADDRESS));
+}
+
+// Checks if the first 4 chars saved in
+// emulated EEPROM are digits. If not,
+// no item was saved at that address
+bool StorageModule::hasAt(int at)
+{
+	String storedLength; // Read the stored length of the variable.
+	read(storedLength, at, at + 4);
+
+	return isAddress(storedLength.c_str());
+}
+
+bool StorageModule::isAddress(const char* addr)
+{
+	for (int i = 0; i < strlen(addr); ++i)
 	{
-		if(!isdigit(length[i]))
+		if(!isdigit(addr[i]))
 		{
 			return false;
 		}
@@ -27,38 +50,89 @@ bool StorageModule::hasJwt()
 	return true;
 }
 
-const char* StorageModule::readJwt()
+const char* StorageModule::readAt(int at)
 {
-	// If in-memory JWT is already stored
-	if (memoryJwt.length() != 0) {
-		return memoryJwt.c_str();
+	// Optimization, if in-memory var is already stored
+	// Take directly from memory
+	String& memoryVar = getMemoryItemByAddress(at);
+	if (memoryVar.length() != 0) {
+		return memoryVar.c_str();
 	}
 
 	// First read the length of the jwt (read the first 4 chars in EEPROM)
-	String jwtLength;
-	read(jwtLength, 0, 4);
+	String storedLength;
+	read(storedLength, at, at + 4);
+
+	// Nothing is stored!
+	if (!isAddress(storedLength.c_str()))
+	{
+		// memoryVar = String(); // Cannot use this because of length check above
+		return memoryVar.c_str();
+	}
 
 	// Then read the actual jwt using the length
-	String jwt;
-	read(jwt, 4, 4 + jwtLength.toInt());
+	String stored;
+	read(stored, at + 4, at + 4 + storedLength.toInt());
 
-	memoryJwt = String(jwt);
-	return memoryJwt.c_str();
+	memoryVar = String(stored);
+	return memoryVar.c_str();
 }
 
-void StorageModule::writeJwt(const char *jwt)
+void StorageModule::writeAt(const char* toWrite, int at)
 {
-	// write the size in the first 4 chars of the EEPROM
-	String jwtSize = String(strlen(jwt));
-	padUntilLength(jwtSize, 4);
-	write(jwtSize.c_str(), 4, 0);
+	// Ignore empty strings
+	if (strlen(toWrite) < 1)
+	{
+		Serial.println("[StorageModule] Skipping empty write.");
+		return;
+	}
 
-	// Then write the actual JWT
-	write(jwt, strlen(jwt), 4);
+	// write the size as 4 chars in the EEPROM
+	String varSize = String(strlen(toWrite));
+	padUntilLength(varSize, 4);
+	write(varSize.c_str(), 4, at);
 
-	memoryJwt = String(jwt);
+	// Then write the actual var
+	write(toWrite, strlen(toWrite), at + 4);
 
-	Serial.println("[StorageModule] Done writing JWT.");
+	// Update memory variable
+	String& memoryVar = getMemoryItemByAddress(at);
+	memoryVar = String(toWrite);
+
+	Serial.println("[StorageModule] Done writing.");
+}
+
+void StorageModule::clearAll()
+{
+	deleteAt(JWT_ADDRESS);
+	deleteAt(SSID_ADDRESS);
+	deleteAt(PASS_ADDRESS);
+	deleteAt(E_USERNAME_ADDRESS);
+	deleteAt(E_PASSWORD_ADDRESS);
+}
+
+// Just deletes the addresses.
+void StorageModule::deleteAt(int at)
+{
+	String& memoryVar = getMemoryItemByAddress(at);
+	memoryVar = String(); // Delete memory copy
+	write("xxxx", 4, at); // Delete stored copy
+}
+
+String& StorageModule::getMemoryItemByAddress(size_t addr)
+{
+	switch (addr) {
+		case JWT_ADDRESS:
+			return memoryJwt;
+		case SSID_ADDRESS:
+			return memorySsid;
+		case PASS_ADDRESS:
+			return memoryPass;
+		case E_USERNAME_ADDRESS:
+			return memoryEUsername;
+		case E_PASSWORD_ADDRESS:
+			return memoryEPassword;
+	}
 }
 
 void StorageModule::padUntilLength(String &string, int length)
@@ -72,15 +146,18 @@ void StorageModule::padUntilLength(String &string, int length)
 
 void StorageModule::read(String &outStr, uint16_t startAddress, uint16_t endAddress)
 {
+	Serial.print("[StorageModule] Read at address ");
+	Serial.print(startAddress);
+
 	// Read the content of emulated-EEPROM
 	int i = startAddress;
 	do
 	{
-		outStr = outStr + String((char)EEPROM.read(i));
+		outStr = outStr + String((char) EEPROM.read(i));
 		i++;
 	} while (i < endAddress);
 
-	Serial.print("[StorageModule] Read (");
+	Serial.print(" (");
 	Serial.print(outStr.length());
 	Serial.print(" chars) ... <");
 	Serial.print(outStr);
@@ -89,14 +166,18 @@ void StorageModule::read(String &outStr, uint16_t startAddress, uint16_t endAddr
 
 void StorageModule::write(const char *value, size_t length, uint16_t startAddress)
 {
-	Serial.print("[StorageModule] Writing (");
+	Serial.print("[StorageModule] Writing at address ");
+	Serial.print(startAddress);
+	Serial.print(" (");
 	Serial.print(length);
 	Serial.print(" chars) ... <");
+
 	for (int i = 0; i < length; ++i)
 	{
 		Serial.print(value[i]);
 		EEPROM.put(startAddress + i, value[i]);
 	}
+	
 	Serial.println(">");
 
 	if (!EEPROM.getCommitASAP())
--- src/storage/StorageModule.hpp
+++ src/storage/StorageModule.hpp
@@ -1,10 +1,32 @@
 #ifndef STORAGE_MODULE_HPP
 #define STORAGE_MODULE_HPP
 
-#define EEPROM_EMULATION_SIZE (2 * 1024)
-#define FLASH_DEBUG            0 // 0-2
+#define EEPROM_EMULATION_SIZE  (1024 + 512)  // (2 * 1024)
+#define FLASH_DEBUG            0             // 0-2
 
 #include <Arduino.h>
+
+// ------------------- Storage Schema -----------------------
+// The way this storage works on the EEPROM
+// is by storing a data length at a known address.
+
+// The data length is always 4 chars (done by the)
+// padUntilLength function.
+
+// The data immediately succeeds this length. Example:
+// 00010helloworld
+// ^             ^
+// 0             13
+
+// The known addresses for the starting length pointers are:
+
+const uint16_t JWT_ADDRESS        = 0;    // 4 (len) + 1024 (JWT)        = 1027 MAX
+const uint16_t SSID_ADDRESS       = 1028; // 4 (len) + 64   (SSID)       = 1095 MAX
+const uint16_t PASS_ADDRESS       = 1096; // 4 (len) + 128  (PASS)       = 1228 MAX
+const uint16_t E_USERNAME_ADDRESS = 1229; // 4 (len) + 128  (E_USERNAME) = 1361 MAX
+const uint16_t E_PASSWORD_ADDRESS = 1362; // 4 (len) + 128  (E_PASSWORD) = 1494 MAX
+
+// ---------------------------------------------------------
 
 namespace paddy
 {
@@ -14,20 +36,35 @@ class StorageModule
 
 private:
     String memoryJwt;
+    String memorySsid;
+    String memoryPass;
+    String memoryEUsername;
+    String memoryEPassword;
 
 public:
     static StorageModule &getInstance();
 
-    // May be empty
-    bool hasJwt();
-    const char* readJwt();
-    void writeJwt(const char* jwt);
+    bool hasInsecure();
+    bool hasPersonal();
+    bool hasEnterprise();
+
+    const char* readAt(int at);
+
+    void writeAt(const char* toWrite, int at);
+
+    void clearAll();
+    void deleteAt(int at);
+
+    String& getMemoryItemByAddress(size_t addr);
 
 private:
     StorageModule() {}
     StorageModule(StorageModule const&);
     StorageModule& operator=(StorageModule const&);
 
+    bool hasAt(int at);
+    bool isAddress(const char* addr);
+    
     void padUntilLength(String &string, int length);
     void read(String &outStr, uint16_t startAddress, uint16_t endAddress);
     void write(const char *value, size_t length, uint16_t startAddress);
